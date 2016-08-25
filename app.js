c61d12abd4af5a43d48f2aed0e81af6db0e7038e
Lisa Hull <lisa@jchull.com>
2016-08-25 13:52:57 -0600

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // if you want to use ES2015
 "use strict";
 
-var dotenv = require('dotenv').config();
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
