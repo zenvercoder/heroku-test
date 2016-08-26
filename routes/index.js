23b70aad2716e8c1cfbea1ab986ddd7635c9285a
Lisa Hull <lisa@jchull.com>
2016-08-26 02:30:43 -0600

    working a lil better this time

--- routes/index.js
+++ routes/index.js
@@ -12,37 +12,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/dog', function(req, res, next) {
-  var num = Math.floor((Math.random()*7) + 1);
+  var num = Math.floor((Math.random()*12) + 1);
   res.render('dog', {
-  srcdog: "/images/dog"+num+".gif"
-    //title: 'Awesome Pet Page',
-    //pet1: 'dog',
-    //pet2: 'cat'
+    dogPic: "/images/dog"+num+".gif",
+    title: 'Awesome Pet Page',
+    pet1: 'dog'
   });
   next();
 });
-//
-//router.get('/kitten', function(req, res, next) {
-//// renders html
-//  res.render('index', {
-//    title: 'Awesome Pet Page',
-//    pet1: 'dog',
-//    pet2: 'cat'
-//  });
-//});
 
 module.exports = router;
 
 
+//$.getJSON("https://www.reddit.com/r/Actualshowerthoughts/.json?jsonp=?", function(data) {
+//  $.each(data.children.data.data.title, function(i,item){
+//    $("<div/>").attr("src", item.data.url).appendTo("#dogThoughts");
+//  });
+//});
 
+//dogThoughts: "https://www.reddit.com/r/Actualshowerthoughts/.json.data.children.data.data.title"
 
-
-// cat1 url http://i.giphy.com/vqPOUf6mzrSve.gif
-// cat2 url http://i.giphy.com/Vj5ZgHbXa3kWY.gif
-// cat3 url http://i.giphy.com/wv3pJQDUfpyY8.gif
-
-//dog1 url http://i.giphy.com/DgzJFvt6StyFi.gif
-// dog2 url http://i.giphy.com/DFmCDPP9tR92o.gif
-// dog3 url http://i.giphy.com/aFOtri4IywD7i.gif
 
 
